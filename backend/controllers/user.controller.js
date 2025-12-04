e068d1a4f6abab5e56ee058e9dc8d6b8d72fde1f
PratapBhingare <pratapbhingare28@gmail.com>
2025-12-04 16:21:51 +0530

    logout route is doen 👍

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,13 +1,12 @@
-const userModel = require('../models/userModel');
-// const blackTokenModel = require('../models/blacklistedTokenModel');
-const userService = require('../services/user.service');
-const {validationResult} = require('express-validator');
-
+const userModel = require("../models/userModel");
+const blackTokenModel = require("../models/blacklistedTokenModel");
+const userService = require("../services/user.service");
+const { validationResult } = require("express-validator");
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
 
   const { fullname, email, password } = req.body;
@@ -15,48 +14,58 @@ module.exports.registerUser = async (req, res, next) => {
   const hashedPassword = await userModel.hashPassword(password);
 
   const user = await userService.createUser({
-      firstname: fullname.firstname,
-      lastname: fullname.lastname,
-      email,
-      password: hashedPassword
+    firstname: fullname.firstname,
+    lastname: fullname.lastname,
+    email,
+    password: hashedPassword,
   });
 
-  if(!user){
-    return res.status(401).json({message:"Duplicate email found!"});
+  if (!user) {
+    return res.status(401).json({ message: "Duplicate email found!" });
   }
 
   const token = user.generateAuthToken();
 
   res.status(201).json({ token, user });
-}
+};
 
 module.exports.loginUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
 
   const { email, password } = req.body;
 
-  const user = await userModel.findOne({ email }).select('+password');
+  const user = await userModel.findOne({ email }).select("+password");
 
   if (!user) {
-      return res.status(401).json({ message: 'Invalid Email or Password' });
+    return res.status(401).json({ message: "Invalid Email or Password" });
   }
 
   const isMatch = await user.comparePassword(password);
 
   if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid Email or Password' })
+    return res.status(401).json({ message: "Invalid Email or Password" });
   }
 
   const token = await user.generateAuthToken();
 
-  res.cookie('token', token);
+  res.cookie("token", token);
 
   res.status(200).json({ token, user });
-}
+};
 
 module.exports.getUserProfile = async (req, res, next) => {
   res.status(200).json(req.user);
-}
+};
+
+module.exports.userLogout = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  await blackTokenModel.create({ token: token });
+
+  res.clearCookie("token");
+
+  res.status(200).json({ message: "logged out successfully!" });
+};
