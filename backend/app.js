6da61c11955d731bf9684782107b2cb9185bbb47
PratapBhingare <pratapbhingare28@gmail.com>
2025-12-05 16:43:17 +0530

    captain routes added in app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const DBConnect = require("./db/db");
-const cors = require('cors');
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const userRoutes = require('./routes/user.routes')
+const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 
 dotenv.config();
 DBConnect();
 
-    const app = express();
+const app = express();
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 app.get("/", (req, res) => {
-    res.send("API is running...");
+  res.send("API is running...");
 });
 
-app.use('/users',userRoutes);
+app.use("/users", userRoutes);
+
+app.use("/captains", captainRoutes);
 
 module.exports = app;
