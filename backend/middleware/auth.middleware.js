cc8d24c780120319089cd4a0185f0f5b022a6f35
PratapBhingare <pratapbhingare28@gmail.com>
2025-12-13 22:04:38 +0530

    captain profile controller done

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/userModel');
 const jwt = require("jsonwebtoken");
 const blackTokenModel = require('../models/blacklistedTokenModel');
+const captainModel = require('../models/captainModel');
 
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
@@ -22,6 +23,32 @@ module.exports.authUser = async (req, res, next) => {
         const user = await userModel.findById(decoded._id);
 
         req.user = user;
+
+        return next();
+
+    } catch (error) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+}
+
+module.exports.authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        res.status(401).json({ message: "Unauthorized user" });
+    }
+
+    const isBlack = await blackTokenModel.findOne({ token: token });
+
+    if (isBlack) {
+        return res.status(401).json({ message: "Unauthorized user" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+
+        req.captain = captain;
 
         return next();
 
