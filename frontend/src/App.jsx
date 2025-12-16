ce7289edc74bb54f4ff13ef021cd800ed9c4874b
PratapBhingare <pratapbhingare28@gmail.com>
2025-12-16 15:00:01 +0530

    routes are made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import UserLogin from "./Pages/UserLogin";
+import Start from "./Pages/start";
+import UserSignup from "./Pages/UserSignup";
+import CaptainLogin from "./Pages/CaptainLogin";
+import CaptainSignup from "./Pages/CaptainSignup";
 
 function App() {
   return (
-    <div className='bg-amber-300'>App</div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Start />}></Route>
+        <Route path="/login" element={<UserLogin />}></Route>
+        <Route path="/signup" element={<UserSignup />}></Route>
+        <Route path="/captain-login" element={<CaptainLogin />}></Route>
+        <Route path="/captain-signup" element={<CaptainSignup />}></Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
