8aa63e1ce6b9c122f8530aff68c79dd3c34713b7
PratapBhingare <pratapbhingare28@gmail.com>
2025-12-22 12:19:33 +0530

    location search panel design

---
+++ frontend/src/Components/LocationSearchPanel.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function LocationSearchPanel() {
+  return (
+    <>
+      <div className='flex justify-start items-center gap-6 text-wrap mb-7'>
+        <div className='h-8 w-15 rounded-full bg-gray-200 flex justify-center items-center'><i className="ri-map-pin-fill"></i></div>
+        <h3 className='text-md font-medium'>Dange Chowk, wakad, pune, 42312d fbajbfaf aksbca  f bevbugdw gdqwdbms dsggsds </h3>
+      </div>
+      <div className='flex justify-start items-center gap-6 text-wrap mb-7'>
+        <div className='h-8 w-15 rounded-full bg-gray-200 flex justify-center items-center'><i className="ri-map-pin-fill"></i></div>
+        <h3 className='text-md font-medium'>Dange Chowk, wakad, pune, 42312d fbajbfaf aksbca  f bevbugdw gdqwdbms dsggsds </h3>
+      </div>
+      <div className='flex justify-start items-center gap-6 text-wrap mb-7'>
+        <div className='h-8 w-15 rounded-full bg-gray-200 flex justify-center items-center'><i className="ri-map-pin-fill"></i></div>
+        <h3 className='text-md font-medium'>Dange Chowk, wakad, pune, 42312d fbajbfaf aksbca  f bevbugdw gdqwdbms dsggsds </h3>
+      </div>
+      <div className='flex justify-start items-center gap-6 text-wrap mb-7'>
+        <div className='h-8 w-15 rounded-full bg-gray-200 flex justify-center items-center'><i className="ri-map-pin-fill"></i></div>
+        <h3 className='text-md font-medium'>Dange Chowk, wakad, pune, 42312d fbajbfaf aksbca  f bevbugdw gdqwdbms dsggsds </h3>
+      </div>
+      <div className='flex justify-start items-center gap-6 text-wrap mb-7'>
+        <div className='h-8 w-15 rounded-full bg-gray-200 flex justify-center items-center'><i className="ri-map-pin-fill"></i></div>
+        <h3 className='text-md font-medium'>Dange Chowk, wakad, pune, 42312d fbajbfaf aksbca  f bevbugdw gdqwdbms dsggsds </h3>
+      </div>
+    </>
+  )
+}
+
+export default LocationSearchPanel
